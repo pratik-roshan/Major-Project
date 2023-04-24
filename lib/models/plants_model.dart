6b13b1bcaef6f28a88dc310dbadbefb11f121e46
Pratik Roshan Shah <pratik.roshan.21@gmail.com>
2023-04-24 13:46:42 +0545

    Plants Model

---
+++ lib/models/plants_model.dart
@@ -0,0 +1,14 @@
+class Plants {
+  final String name;
+  final String sname;
+  final String property;
+  final String location;
+  final String photo;
+
+  Plants(
+      {required this.name,
+      required this.sname,
+      required this.property,
+      required this.location,
+      required this.photo});
+}
